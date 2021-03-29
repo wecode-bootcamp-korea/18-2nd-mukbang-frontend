63ef0bb0ddfa75831de09b9d81d876057ca008ee
Joung seoung ok <joungseoungok@Joungui-MacBookPro.local>
2021-03-29 19:20:23 +0900

    초기세팅 수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Main from "./Pages/Main/Main";
-import Login from "./Pages/Login/Login";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Main from './Pages/Main/Main';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact="/login" component={Login} />
-        <Route exact="/main" component={Main} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/main" component={Main} />
       </Switch>
     </Router>
   );
