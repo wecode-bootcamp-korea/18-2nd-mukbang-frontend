d9947791bfebb287b3d4f91ee66233856caa4888
Joung seoung ok <joungseoungok@Joungui-MacBookPro.local>
2021-04-01 15:24:23 +0900

    [18기 정승옥] 헤더 레이아웃, hover, link path 작업
    [18기 정승옥] Conflict 해결

--- src/Routes.js
+++ src/Routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Main from './Pages/Main/Main';
 import Login from './Pages/Login/Login';
+import Header from './Components/Header/Header';
 
-function App() {
+function App(props) {
   return (
     <Router>
+      <Header component={Header} />
       <Switch>
         <Route exact path="/login" component={Login} />
         <Route exact path="/main" component={Main} />
